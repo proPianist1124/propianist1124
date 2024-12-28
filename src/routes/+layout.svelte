2f17a9f63d82277afb15a053dd21197fae3c2aa6
proPianist1124 <phantomforces11242009@gmail.com>
2024-12-27 20:05:13 -0800

    updated favicon instead of svelte default

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,10 @@
 <script>
+    import favicon from "$lib/assets/favicon.ico";
     import "$lib/assets/style.css";
 </script>
+
+<svelte:head>
+    <link rel="icon" type="image/x-icon" href={favicon}>
+</svelte:head>
+
 <slot />
